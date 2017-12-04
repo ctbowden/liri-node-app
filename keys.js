22967c774711a011b8e20ed522aacc5300bb9ef8
Charles Thomas Bowden <charles.bowden@gmail.com>
2017-12-04 14:08:53 -0500

    Initial Site Config

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '	qMVw6JsxurIuIp2qhfFacNfft',
+  consumer_secret: 'xNscBUtoqh2qIgLhL3ry2Pz31CV3SFtwsjcdbRL0tiyM8fhw3j',
+  access_token_key: '937752798267133952-YYu28BDhvZvMljkuaLb3LIldABbPnRy',
+  access_token_secret: 'IZsnX8XJiSyAhzkbG6MppwxhOUbEVOVIVITCi2J3popLq',
+}
+
+module.exports = twitterKeys;
