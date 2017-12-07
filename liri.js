a78bca337c0d02fbdc1c4ee8ce36b1fddf3a5697
Charles Thomas Bowden <charles.bowden@gmail.com>
2017-12-07 16:35:54 -0500

    Improved Handling of missing search arguments

--- liri.js
+++ liri.js
@@ -25,53 +25,15 @@ var reqTitle = userRequest[3];
 // Switch Statement to process user functionality
 switch(reqFunction) {
 	case "my-tweets":
-		// Call function for Twitter
-		var client = new twitter({
-  			consumer_key: twitterKeys.consumer_key,
-  			consumer_secret: twitterKeys.consumer_secret,
-  			access_token_key: twitterKeys.access_token_key,
-  			access_token_secret: twitterKeys.access_token_secret
-		});
-
-		// Get Tweets "statuses/user_timeline" looks at the authenticated user and grabs updates from their timeline 
-		client.get('statuses/user_timeline', function(error, tweets, response) {
-   			// Handle the Error
-   			if(error){
-   				console.log(error);
-   			}   			
-   			// No error then cycle through tweets and display in console
-   			for (var i = 0; i < tweets.length; i++) {
-   				console.log("--------");
-   				console.log(tweets[i].created_at);
-   				console.log(tweets[i].text);
-   				console.log("--------");
-   			};
-		});
+		tweets();
 		//End Process
 		break;
 	case "spotify-this-song":
-		if (reqTitle === undefined) {
-			reqTitle = "The Sign Ace of Base";
-		}
-
 		song(reqTitle);
-
 		//Break
 		break;
 	case "movie-this":
-		if (reqTitle === undefined) {
-			reqTitle = "Mr. Nobody";
-		}
-		// Call function for OMDB
-		request('http://www.omdbapi.com/?t=' + reqTitle + '&y=&plot=short&apikey=trilogy', function(error, response, body) {
-			// If the request is successful (i.e. if the response status code is 200)
-  			if (!error && response.statusCode === 200) {
-			// Parse the body of the site and recover needed information about movie
-			console.log("----- Your Movie Information -----");
-    		console.log("Title of the movie: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating of the Movie: "+ JSON.parse(body).imdbRating + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry of Production: " + JSON.parse(body).Country + "\nLanguage: "+ JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nCast: "+ JSON.parse(body).Actors);
-    		console.log("----- Enjoy the Show -----");
-  			}
-		});
+		movie(reqTitle);
 		//Break
 		break;
 	case "do-what-it-says":
@@ -84,16 +46,68 @@ switch(reqFunction) {
 			var doWhat = dataArr[0];
 			var doThis = dataArr[1];
 
-			if (doWhat === "spotify-this-song"){
-				song(doThis);
+			switch(doWhat){
+				case "spotify-this-song":
+					song(doThis);
+					break;
+				case "my-tweets":
+					tweets();
+					break;
+				case "movie-this":
+					movie(doThis);
+					break;
 			};
-		})
+		});
 		//Break
 		break;
 }
 
+function tweets(){
+	// Call function for Twitter
+	var client = new twitter({
+  		consumer_key: twitterKeys.consumer_key,
+  		consumer_secret: twitterKeys.consumer_secret,
+  		access_token_key: twitterKeys.access_token_key,
+  		access_token_secret: twitterKeys.access_token_secret
+	});
+	// Get Tweets "statuses/user_timeline" looks at the authenticated user and grabs updates from their timeline 
+	client.get('statuses/user_timeline', function(error, tweets, response) {
+   	// Handle the Error
+   		if(error){
+   			console.log(error);
+   		}   			
+   		// No error then cycle through tweets and display in console
+   		for (var i = 0; i < tweets.length; i++) {
+   			console.log("--------");
+   			console.log(tweets[i].created_at);
+   			console.log(tweets[i].text);
+   			console.log("--------");
+   		};
+	});
+}
+
+//Movie This Function
+function movie(arr) {
+	if (!arr) {
+		arr = "Mr. Nobody";
+	}
+	// Call function for OMDB
+	request('http://www.omdbapi.com/?t=' + arr + '&y=&plot=short&apikey=trilogy', function(error, response, body) {
+		// If the request is successful (i.e. if the response status code is 200)
+  		if (!error && response.statusCode === 200) {
+		// Parse the body of the site and recover needed information about movie
+		console.log("----- Your Movie Information -----");
+    	console.log("Title of the movie: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating of the Movie: "+ JSON.parse(body).imdbRating + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry of Production: " + JSON.parse(body).Country + "\nLanguage: "+ JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nCast: "+ JSON.parse(body).Actors);
+    	console.log("----- Enjoy the Show -----");
+  		}
+	});
+}
+
 //Spotify Function outside of switch statement to make it easier to handle "do-what-it-says"
 function song(arr) {
+	if (!arr) {
+		arr = "The Sign Ace of Base";
+	}
 	// Call function for Spotify
 	var spotify = new Spotify({
   		id: "ebe3b059468f47a1b09765a5d02d7a1c",
